e791890b088c9d3fdd9628d773938fed3a11b46c
yuliacannon <yuliapushka@gmail.com>
2019-02-06 16:01:18 +0200

    complete

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,36 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"]
 })
 export class ProfileComponent implements OnInit {
-
   id: string;
   response: any;
+  responseR: any;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   getUser() {
-    this.http.get('https://api.github.com/users/' + this.id)
-    .subscribe((response)=>{
-      this.response = response;
-      console.log(this.response);
-    })
-    // this.http.get('https://api.github.com/users/' + this.id+'/repos')
-    // .subscribe((response)=>{
-    //   this.response = response;
-    //   console.log(this.response);
-    // })
+    this.http
+      .get("https://api.github.com/users/" + this.id)
+      .subscribe(response => {
+        this.response = response;
+        console.log(this.response);
+      });
   }
 
+  getRepos() {
+    this.http
+      .get("https://api.github.com/users/" + this.id + "/repos")
+      .subscribe(responseR => {
+        this.responseR = responseR;
+        console.log(this.responseR);
+      });
+  }
 
   ngOnInit() {
-    this.id = this.route.snapshot.paramMap.get('id');
+    this.id = this.route.snapshot.paramMap.get("id");
     this.getUser();
+    this.getRepos();
   }
-
 }
